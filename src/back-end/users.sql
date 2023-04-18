941bd500b851f7b63a9a44d514549c602e0f6ff2
Fezz <jonnicwolf@gmail.com>
2023-04-18 10:45:23 -0400

    database setup, not done

---
+++ src/back-end/users.sql
@@ -0,0 +1,18 @@
+CREATE TABLE users (
+  user_id INT PRIMARY KEY,
+  username VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE passwords (
+  passowrd INT PRIMARY KEY,
+  password_hash varchar(255) NOT NULL,
+  user_id INT,
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+
+INSERT INTO users (user_id, username)
+VALUES
+  (1, 'admin');
+
+INSERT INTO passwords (user_id,)
