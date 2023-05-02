39a82ce8a6a49d41a2f44c0983f44636401d7cec
Fezz <jonnicwolf@gmail.com>
2023-05-02 12:26:22 -0400

    env

--- back-end/app.js
+++ back-end/app.js
@@ -2,7 +2,9 @@ const cors = require('cors');
 const express = require('express');
 
 const app = express();
+const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
 
+module.exports = app;
