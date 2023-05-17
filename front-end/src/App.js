168355fb6bdf1cfdad9e2c3d55dfe646910310ba
Fezz <jonnicwolf@gmail.com>
2023-05-17 11:49:06 -0400

    housekeeping

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import TipSheet from './components/TipSheet';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <>
-      <TipSheet/>
+      <NavBar/>
+      <Routes>
+        <Route path='/' element={<TipSheet/>} />
+        {/* <Route path='/manuals' element={<Manuals/>} />
+        <Route path='/balance-register' element={<BalanceRegister/>} /> */}
+      </Routes>
     </>
   )
 };
