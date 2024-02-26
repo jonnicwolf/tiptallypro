80124ed801086ca51efd9421aae20f2c3c72259c
Fezz <jonnicwolf@gmail.com>
2024-02-26 13:42:25 -0500

    adds copyright and does some cleanup

--- src/components/assets/follylogo.js
+++
@@ -1,3 +0,0 @@
-const follyLogo = require('https://images.squarespace-cdn.com/content/v1/560d8157e4b0dfb263d37f7d/1447992635371-E29KM9C6YNABAP5MSETF/logo2.png?format=1500w')
-
-export default follyLogo;
